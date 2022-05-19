b0985d69a031a905d64ed892f88d6e8e55c8050d
gkdms3082@naver.com <gkdms3082@naver.com>
2022-05-19 10:43:52 +0900

    7장까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,56 +1,49 @@
 import React from 'react';
-
-function Food({name,picture}){
-  return(
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture} />
-    </div>
-  ); 
-}
-
-const foodILike = [
-  {
-    id: 1
-    name: 'Kimchi',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.1store.io%2FofficialProductImage%2F1587198787425-0712162682417814.png&f=1&nofb=1'
-    rating: 5
-  },
-  {
-    id: 2
-    name: 'Samgyeopsal',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR800x0%2F%3Fscode%3Dmtistory2%26fname%3Dhttps%3A%252F%252Fblog.kakaocdn.net%252Fdn%252FdDyTJN%252FbtqvrVnOCb4%252FVDVFUo7oXVnoFqlVIzH0h0%252Fimg.png&f=1&nofb=1'
-    rating: 5
-  },
-  {
-    id: 3
-    name: 'Bibimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frecipe1.ezmember.co.kr%2Fcache%2Frecipe%2F2018%2F10%2F03%2F122c9216ad8548da2c60ca8c8a5f4a1e1.jpg&f=1&nofb=1'
-    rating: 5
-  },
-  {
-    id: 4
-    name: 'Doncasu',
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fthumb%2FR1280x0.fjpg%2F%3Ffname%3Dhttp%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fservice%2Fuser%2F3acD%2Fimage%2FSKsjpw3oIvlW058vB5DOfaeIUOQ.jpg&f=1&nofb=1'
-    rating: 5
-  },
-  {
-    id: 5
-    name: 'Kimbap',
-    image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Frecipe1.ezmember.co.kr%2Fcache%2Frecipe%2F2018%2F12%2F14%2Ff609b820823418706dca1df13c43def01.jpg&f=1&nofb=1'
-    rating: 5
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+class App extends React.Component{
+  state = {
+    isLoading:true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading: false});
   }
-];
-
-function renderFood(dish){
-  return <Food key={dish.name} name= {dish.name} picture={dish.image} />;
-}
-function App() {
-  return (
-    <div>
-      {foodILike.map(renderFood)}
-    </div>
-  );
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state; 
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+             />
+            ))}
+          </div>
+        )}
+        </section>
+    );
+  }
 }
 
 export default App;
